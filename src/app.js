d70613acfc8c5e3a14d603d58268402d786b20c8
glyh0515 <1155176301@link.cuhk.edu.hk>
2023-03-23 00:38:51 +0800

    Login and Register Page

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './login'
+import Register from './register'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
