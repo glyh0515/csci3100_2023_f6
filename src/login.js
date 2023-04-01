82fc4141e6d7ebf747ffd361b109ed492ffc0d2e
glyh0515 <1155176301@link.cuhk.edu.hk>
2023-04-01 22:52:28 +0800

    Change in style

--- src/login.js
+++ src/login.js
@@ -7,7 +7,7 @@ function Login() {
             <div className="container h-100">
                 <div className="d-flex justify-content-center align-items-center h-100">
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
+                        <div className="card bg-dark text-white" style={{ borderRadius: '2rem' }}>
                             <div className="card-body p-5 text-center">
                                 <div className="mb-md-5 mt-md-4 pb-5">
                                     <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
