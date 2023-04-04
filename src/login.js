9f1928ddfbba13a94ce41fc667bbea2cf720864b
HarryKu2001 <1155160201@link.cuhk.edu.hk>
2023-04-05 04:45:36 +0800

    Update login.js

--- src/login.js
+++ src/login.js
@@ -6,10 +6,10 @@ function Login() {
            <h2>Login</h2>
            <p>Please enter your email and password!</p>
             <div>
-                <input type="email" placeholder="Enter your email"/>
+                Email: <input type="email" placeholder="Enter your email"/>
             </div>
             <div>
-                <input type="password" placeholder="Enter your password"/>
+                Password: <input type="password" placeholder="Enter your password"/>
             </div>
             <button type="submit">Login</button>
             <p>Don't have an account?<a href="http://localhost:3000/register">Click here</a>
