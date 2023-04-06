ca3441d23cec9a7e5db9065db571e0a8d56d68fb
HarryKu2001 <1155160201@link.cuhk.edu.hk>
2023-04-06 22:18:22 +0800

    Change style and CSS

--- src/login.js
+++ src/login.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import './CSS/Login_SignUp.css';
 
 function Login() {
     return (
-        <form>
-           <h2>Login</h2>
-           <p>Please enter your email and password!</p>
-            <div>
-                Email: <input type="email" placeholder="Enter your email"/>
-            </div>
-            <div>
-                Password: <input type="password" placeholder="Enter your password"/>
-            </div>
-            <button type="submit">Login</button>
-            <p>Don't have an account?<a href="http://localhost:3000/register">Click here</a>
-            </p>
-        </form>
+        <div style={{backgroundColor:'#C7B9B4', textAlign:'left'}}>
+            <form>
+            <h2>Login</h2>
+            <p>Please enter your email and password!</p>
+                <div>
+                    Email: <input type="email" placeholder="Enter your email"/>
+                </div>
+                <div>
+                    Password: <input type="password" placeholder="Enter your password"/>
+                </div>
+                <button type="submit">Login</button>
+                <p>Don't have an account?<a href="http://localhost:3000/register">Click here</a>
+                </p>
+            </form>
+        </div>
     );
 }
 
