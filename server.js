939348c10a28aae0f55e19642b7bf6bae75d99ae
glyh0515 <1155176301@link.cuhk.edu.hk>
2023-04-10 22:20:21 +0800

    Server update

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
+const methodOverride = require('method-override')
 
 const app = express();
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000,
-	max: 100,
-	standardHeaders: true,
-	legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
 })
 
 app.use(limiter);
@@ -24,48 +25,53 @@ app.use(xss());
 app.use(hpp());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/my_database', {
+mongoose.connect('mongodb+srv://csci3100_2023_f6:La39HykFMfj2xPK5@cluster0.05afckq.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Define a schema for a sample model
-const sampleSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-});
+// mongoose.connection is an instance of the connected DB
+const db = mongoose.connection;
+// Upon connection failure
+db.on('error', console.error.bind(console, 'connection error:'));
+// Upon opening the database successfully
+db.once('open', function () {
+  console.log("Connection is open...");
+  // Define a schema for a sample model
+  const sampleSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+  });
+  // Create a model based on the schema
+  const Sample = mongoose.model('Sample', sampleSchema);
+  
+  // This module is for parsing the content in a request body (installed with npm)
+  const bodyParser = require('body-parser');
+  // Use parser to obtain the content in the body of a request
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body['_method'];
+      delete req.body['_method'];
+      return method;
+    }
+  }));
 
-// Create a model based on the schema
-const Sample = mongoose.model('Sample', sampleSchema);
-
-// Define a route that creates a new sample document
-app.get('/create-sample', async (req, res) => {
-  try {
-    const sample = new Sample({
-      name: 'John Doe',
-      age: 30,
+  // Define a route that creates a new sample document
+  app.get('/create-sample', (req, res) => {
+    Sample.create({
+      name: "John Doe",
+      age: 30
     });
+    res.send("Document created!");
+    console.log("Document created!");
+  });
 
-    await sample.save();
-
-    res.send('Sample created!');
-  } catch (error) {
-    res.status(500).send('Internal server error');
-  }
-});
-
-// Start the server
-const server = app.listen(8080, () => {
-  console.log('Server started on port 8080');
-});
-
-// Set up graceful shutdown for the server
-process.on('SIGTERM', () => {
-  server.close(() => {
-    console.log('Server shut down gracefully');
+  app.all('/*', (req, res) => {
+    res.status(404).redirect('http://localhost:3000/');
   });
 });
 
-app.all('/*', (req, res) => {
-    res.status(404).redirect('http://localhost:3000/');
-});
+// Start the server
+const server = app.listen(8080);
