85c36aa898298ed116fbcda1cb2f3be19e6e752b
ansonlk48 <63101547+ansonlk48@users.noreply.github.com>
2023-04-13 02:01:14 +0800

    update login validation

--- server.js
+++ server.js
@@ -112,29 +112,6 @@ db.once('open', function () {
     }
   });
 
-  app.post('/login', async (req, res) => {
-    try {
-      // Check if user exists in the database
-      const student = await Student.findOne({ Email: req.body.email });
-      if (!student) {
-        return res.status(400).send('Invalid email.');
-      }
-      // Check if the provided password is valid
-      const validPassword = await bcrypt.compare(req.body.password, student.Password);
-      if (!validPassword) {
-        return res.status(400).send('Invalid password.');
-      }
-      const token = jwt.sign({ _id: student._id }, process.env.TOKEN_SECRET);
-      res.header('x-auth-token', token);
-
-      // Redirect to the profile page on successful login
-      res.redirect("http://localhost:3000/profile");
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal server error.');
-    }
-  });
-
   // Posting a register request for student
   app.post('/user/register', async (req, res) => {
     //hash the password
@@ -142,19 +119,19 @@ db.once('open', function () {
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
     try {
       await User.create({
-        StudentID: req.body['sid'],
-        Name: req.body['name'],
-        Email: req.body['email'],
-        Password: hashedPassword,
-        Major: req.body['major'],
-        Year: req.body['year']
-      });
-      res.status(200).json({ message: 'Student registered successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error registering the student' });
-    }
-  });
+      StudentID: req.body['studentID'],
+      Name: req.body['name'],
+      Email: req.body['email'],
+      Password: hashedPassword,
+      Major: req.body['major'],
+      Year: req.body['year'],
+    });
+    res.status(200).json({ message: 'Student registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error registering the student' });
+  }
+});
 
   app.post('/admin/register', async (req, res) => {
     //hash the password
@@ -210,7 +187,66 @@ db.once('open', function () {
   });
 });
 
+  app.post('/delete-user/:studentID', async (req, res) => {
+    try {
+      await Student.findOneAndDelete({StudentID: req.params['studentID']});
+      res.status(200).json({ message: 'Student deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error deleting the student' });
+    }
+  });
 
+  app.post('/delete-admin/:adminID', async (req, res) => {
+    try {
+      await Admin.findOneAndDelete({AdminID: req.params['adminID']});
+      res.status(200).json({ message: 'Admin deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error deleting the admin' });
+    }
+  });
+
+  app.post('/delete-course/:courseID', async (req, res) => {
+    try {
+      await Course.findOneAndDelete({CourseID: req.params['courseID']});
+      res.status(200).json({ message: 'Course deleted successfully' });
+      console.log(req.params['courseID'], "deleted!");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error deleting the course' });
+    }
+  });
+
+  app.get('/', (req, res) => {
+    res.redirect("http://localhost:3000/login");
+  });
+
+app.post('/login', async (req, res) => {
+  try {
+    // Check if user exists in the database
+    const user = await User.findOne({ Email: req.body.email });
+    if (!user) {
+      res.status(400).json({ message: 'Invalid email or password' });
+    }
+    // Check if the provided password is valid
+    const validPassword = await bcrypt.compare(req.body.password, user.Password); 
+    if (!validPassword) {
+      res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('x-auth-token', token);
+
+    // Redirect to the profile page on successful login
+    res.redirect("http://localhost:3000/profile");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error.');
+  }
+});
+
+
+  
 
 // Start the server
 const server = app.listen(8080);
